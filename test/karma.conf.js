6ee5296f006f153b179e5e31760f15eef82af82f
Rafael Martins <rafaelwmartins@gmail.com>
2014-04-12 12:54:56 -0300

    Travis CI

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function(config) {
 
         plugins: [
             'karma-chrome-launcher',
+            'karma-firefox-launcher',
             'karma-jasmine'
         ]
     });
